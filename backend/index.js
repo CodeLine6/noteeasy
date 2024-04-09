e26aaf7d16a3af50277681d41628d6de3cd03bcb
missingsemicolononline8 <76899808+missingsemicolononline8@users.noreply.github.com>
2024-04-10 05:10:32 +0530

    Google callback url fix

--- backend/index.js
+++ backend/index.js
@@ -27,7 +27,7 @@ app.use(passport.initialize());
 passport.use(new GoogleStrategy({
     clientID: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: '/api/auth/google/callback'
+    callbackURL: 'https://noteeasy.onrender.com/api/auth/google/callback'
 },
     async (accessToken, refreshToken, profile, done) => {
         try {
